63acfed3a7bef83dfe1b59190478e7f16f03cca4
Dolev Hadar <dolevc2@gmail.com>
2022-12-17 15:23:32 +0200

    feat: typography
    Signed-off-by: Dolev Hadar <dolevc2@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
 
 function App() {
-  return <div className={styles.root}>Test</div>;
+  return (
+    <div className={styles.root}>
+      <h1>Start by pasting the repository URL.</h1>
+      <p>Test!</p>
+    </div>
+  );
 }
 
 export default App;
